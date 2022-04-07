24f382f5d0411c04f4c1082690d8973c69ac2738
AndrewLevada <levada.andrew@gmail.com>
2022-04-07 21:58:08 +0300

    chore(): added cookies

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
 	originWhitelist: [], // Allow all origins
 	requireHeader: ['origin', 'x-requested-with'],
-	removeHeaders: ['cookie', 'cookie2']
+	removeHeaders: ['cookie', 'cookie2'],
+	setHeaders: {"cookie": "PHPSESSID=t5iujrsebk7iq5nrj61bmf5san; OP_SAMSON_GUEST_ID=92201813; BX_USER_ID=7ef298cdd6d4fcb20866cc3a9d44e713;"},
 }).listen(port, host, function() {
 	console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
